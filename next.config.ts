984eec0dfbbfdeac70bcf9d69c84f5f40915058b
huavcjj <huavcjj0217@icloud.com>
2025-03-02 21:04:49 +0900

    feat

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: true,
+  },
+  // experimental: {
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
